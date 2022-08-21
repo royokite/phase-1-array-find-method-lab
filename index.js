01733ccd542e666d2b73a0927e00082d14200e09
RoyOkite <royokite@gmail.com>
2022-08-21 15:53:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// Array find method lab!
+
+function superbowlWin(record) {
+    const result = record.find(item => item.result==='W');
+    
+    if (!!result===true) return result.year;
+    else return result;
+}
